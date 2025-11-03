0125ff3233272bbb540faea4b6889fb00f2a0bf9
Victor Silva Guedes <sguedes.victor@gmail.com>
2025-11-03 08:28:00 -0300

    feat: add swagger

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,10 @@ import familiarRouter from './routes/familiar.routes'
 import profissionalRouter from './routes/profissional.routes'
 import internacaoRouter from './routes/internacao.routes'
 import evolucaoRouter from './routes/evolucao.routes'
+import associacaoRouter from './routes/associacao.routes'
+
+import swaggerUi from 'swagger-ui-express'
+import * as swaggerDocument from '../swagger.json'
 
 const app: Express = express()
 
@@ -15,11 +19,13 @@ app.use(cors())
 app.use(express.json())
 
 // --- Rotas ---
+// 3. ROTA DE DOCUMENTAÇÃO (Swagger)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 // Rota "Health Check"
 app.get('/', (req: Request, res: Response) => {
 	res.status(200).json({
-		message: 'InfoCare API está online!',
+		message: 'InfoCare API está online! Documentação em /api-docs',
 		timestamp: new Date().toISOString(),
 	})
 })
@@ -29,7 +35,8 @@ app.use('/pacientes', pacienteRouter)
 app.use('/familiares', familiarRouter)
 app.use('/profissionais', profissionalRouter)
 app.use('/internacoes', internacaoRouter)
-app.use('/evolucao', evolucaoRouter)
+app.use('/evolucoes', evolucaoRouter)
+app.use('/associacoes', associacaoRouter)
 
 // --- Tratamento de Erros (NOVO) ---
 //
