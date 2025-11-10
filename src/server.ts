42f7c7c8cfd6417a991f66433edfb90b1c9c4030
Victor Silva Guedes <sguedes.victor@gmail.com>
2025-11-10 11:31:29 -0300

    feat: port number render

--- src/server.ts
+++ src/server.ts
@@ -4,13 +4,16 @@ import 'dotenv/config'
 
 import { app } from './app'
 
-// 2. Define a porta da API
-// Busca a variável 'PORT' no .env ou usa 3001 como padrão
+// 2. O Render define process.env.PORT. Se não existir (no dev local), usamos 3333.
 const PORT = process.env.PORT || 3333
 
-// 3. Inicia o servidor
-app.listen(PORT, () => {
-	console.log(
-		`InfoCare API rodando em: http://localhost:${PORT} 🏥🖥️\nDocumentação em http://localhost:${PORT}/api-docs`
-	)
+// 3. Iniciamos o servidor na porta correta.
+// O Node.js automaticamente escuta em '0.0.0.0' por padrão
+// quando executado em ambientes como o Render, o que é o correto.
+app.listen(Number(PORT), () => {
+	// 4. Corrigimos a mensagem de log
+	console.log(`InfoCare API rodando na porta ${PORT} 🏥🖥️`)
+
+	// (O link da documentação ainda será útil localmente)
+	console.log(`Documentação (local): http://localhost:${PORT}/api-docs`)
 })
